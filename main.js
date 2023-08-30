f45e46e6c01d16a4f37db26e37faf10c7b027982
aldorobi <97127948+aldorobi@users.noreply.github.com>
2023-08-30 15:46:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function rafikiboton(){
+    document.getElementById("rafikisonido").play()
+}
+
+function timonboton(){
+    document.getElementById("timonsonido").play()
+}
+
+
+function pandaFuncion(){
+    document.getElementById("pandasonido").play()
+}
+
+
+function scatboton(){
+    document.getElementById("ardillasonido").play()
+}
+
+
+function gatoboton(){
+    document.getElementById("gatosonido").play()
+}
